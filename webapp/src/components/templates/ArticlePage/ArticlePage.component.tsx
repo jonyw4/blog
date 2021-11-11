3bb24a71335df4eb2c51bd174098cf5f31ee6a40
Jonathan Célio <jonathan.clio@hotmail.com>
2021-11-11 17:47:43 -0300

    refactor: update dates from article page template

--- webapp/src/components/templates/ArticlePage/ArticlePage.component.tsx
+++ webapp/src/components/templates/ArticlePage/ArticlePage.component.tsx
@@ -1,10 +1,11 @@
 import { ArticleProps } from "./ArticlePage.props";
 
-export function ArticlePage({ title, children, createdAt }: ArticleProps) {
+export function ArticlePage({ title, children, createdAt, updatedAt }: ArticleProps) {
   return (
     <article>
       <h1 data-test="title">{title}</h1>
       <div>{createdAt}</div>
+      <div>{updatedAt}</div>
       <hr />
 
       <main>{children}</main>
