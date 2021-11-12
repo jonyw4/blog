b8694ac8b32f3b36b4e107194a09c0bcbeb11bd1
Jonathan Célio <jonathan.clio@hotmail.com>
2021-11-11 23:10:14 -0300

    chore: add css to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,6 @@
+import "normalize.css";
+import "../webapp/src/components/styles/global.css";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +9,4 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
