0e766d8275ae2c1904d6029ba6adf9e7d4850d8b
Jonathan Célio <jonathan.clio@hotmail.com>
2021-11-12 00:19:32 -0300

    feat: add footer component

---
+++ webapp/src/components/organisms/Footer/Footer.component.tsx
@@ -0,0 +1,13 @@
+export function Footer(){
+  return (
+    <footer>
+      <small>
+        <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">
+          CC BY-NC-SA 4.0 2021
+        </a>
+        {" "}
+        © Jonathan Celio
+      </small>
+    </footer>
+  );
+}
