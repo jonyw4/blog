169faa018698c27c653d0092209022ed91185205
Jonathan Célio <jonathan.clio@hotmail.com>
2021-11-12 15:17:56 -0300

    refactor: update index page

--- webapp/src/pages/index.tsx
+++ webapp/src/pages/index.tsx
@@ -1,30 +1,21 @@
 import { GetStaticProps } from "next";
 import { formatDate } from "../components/global/formatDate";
 import { articleRepository } from "../factories";
-import { HomePage } from "../components/templates/HomePage";
+import { HomePage, HomePageProps } from "../components/templates/HomePage";
 
-function Home(props: HomeProps) {
+function Home(props: HomePageProps) {
   return <HomePage {...props} />;
 }
 
-export interface HomeProps {
-  articles: Array<{
-    slug: string;
-    title: string;
-    createdAt: string;
-    updatedAt: string;
-  }>;
-}
-
-export const getStaticProps: GetStaticProps<HomeProps> = async () => {
-  const articles = await articleRepository.getAllArticleMetadata()
+export const getStaticProps: GetStaticProps<HomePageProps> = async () => {
+  const articles = await articleRepository.getAllArticleMetadata();
   return {
     props: {
-      articles: articles.map(article => ({
+      articlesPreview: articles.map((article) => ({
         ...article,
         createdAt: formatDate(new Date(article.createdAt)),
         updatedAt: formatDate(new Date(article.updatedAt)),
-      }))
+      })),
     },
   };
 };
