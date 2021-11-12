90282806bbe926c8e8a60643e533040ed02c66cb
Jonathan Célio <jonathan.clio@hotmail.com>
2021-11-12 14:50:10 -0300

    refactor: update index page

--- webapp/src/pages/index.tsx
+++ webapp/src/pages/index.tsx
@@ -1,5 +1,32 @@
-function HomePage() {
-  return <h1 data-test="title">Hello world</h1>;
+import { GetStaticProps } from "next";
+import { formatDate } from "../components/global/formatDate";
+import { articleRepository } from "../factories";
+import { HomePage } from "../components/templates/HomePage";
+
+function Home(props: HomeProps) {
+  return <HomePage {...props} />;
 }
 
-export default HomePage;
+export interface HomeProps {
+  articles: Array<{
+    slug: string;
+    title: string;
+    createdAt: string;
+    updatedAt: string;
+  }>;
+}
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const articles = await articleRepository.getAllArticleMetadata()
+  return {
+    props: {
+      articles: articles.map(article => ({
+        ...article,
+        createdAt: formatDate(new Date(article.createdAt)),
+        updatedAt: formatDate(new Date(article.updatedAt)),
+      }))
+    },
+  };
+};
+
+export default Home;
