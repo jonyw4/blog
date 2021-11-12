8a8118161ef9b0fc9876aaa731a503a3d7d36ffa
Jonathan Célio <jonathan.clio@hotmail.com>
2021-11-12 14:20:58 -0300

    refactor: update article page component to use layout

--- webapp/src/components/templates/ArticlePage/ArticlePage.component.tsx
+++ webapp/src/components/templates/ArticlePage/ArticlePage.component.tsx
@@ -1,18 +1,15 @@
 import { ArticleProps } from "./ArticlePage.props";
-import { Header } from '../../organisms/Header'
-import { Footer } from "../../organisms/Footer";
+import { Layout } from "../../organisms/Layout";
 
 export function ArticlePage({ title, children, createdAt, updatedAt }: ArticleProps) {
   return (
-    <>
-      <Header />
+    <Layout>
       <article>
         <h1 data-test="title">{title}</h1>
         <small>{createdAt}</small>
 
         <main>{children}</main>
       </article>
-      <Footer />
-    </>
+    </Layout>
   );
 }
