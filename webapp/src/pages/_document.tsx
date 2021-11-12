54fe7b20ef4aa77a067bc9ec5f2035f6b9b97f37
Jonathan Célio <jonathan.clio@hotmail.com>
2021-11-11 23:08:55 -0300

    feat: add fonts in document component next

---
+++ webapp/src/pages/_document.tsx
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Athiti:wght@400;600&family=Fira+Sans:ital,wght@1,200&family=Kameron:wght@700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
