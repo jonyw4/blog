e666259780cdcb08e854cfa98da97d7a9fdc71e1
Jonathan Célio <jonathan.clio@hotmail.com>
2021-11-18 16:17:27 -0300

    feat: add button to change locale in header

--- webapp/src/components/organisms/Header/Header.component.tsx
+++ webapp/src/components/organisms/Header/Header.component.tsx
@@ -1,8 +1,19 @@
 import { ProfileCard } from '../ProfileCard'
+import { useLocale } from '../../global'
+
 export function Header(){
+  const { changeLocale } = useLocale()
   return (
     <header>
       <ProfileCard />
+      <ul>
+        <li>
+          <button onClick={() => changeLocale("en-US")}>US</button>
+        </li>
+        <li>
+          <button onClick={() => changeLocale("pt-BR")}>PT-BR</button>
+        </li>
+      </ul>
     </header>
   );
 }
