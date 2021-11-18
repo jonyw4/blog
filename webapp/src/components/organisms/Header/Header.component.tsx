76847b755a496431ab1c1a7a944656d304e705a1
Jonathan Célio <jonathan.clio@hotmail.com>
2021-11-18 17:45:07 -0300

    refactor: add language select component to header

--- webapp/src/components/organisms/Header/Header.component.tsx
+++ webapp/src/components/organisms/Header/Header.component.tsx
@@ -1,19 +1,11 @@
 import { ProfileCard } from '../ProfileCard'
-import { useLocale } from '../../global'
+import { LanguageSelect } from '../../atoms/LanguageSelect/LanguageSelect.component';
 
 export function Header(){
-  const { changeLocale } = useLocale()
   return (
     <header>
       <ProfileCard />
-      <ul>
-        <li>
-          <button onClick={() => changeLocale("en-US")}>US</button>
-        </li>
-        <li>
-          <button onClick={() => changeLocale("pt-BR")}>PT-BR</button>
-        </li>
-      </ul>
+      <LanguageSelect />
     </header>
   );
 }
