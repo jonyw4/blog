52c0203801b6531ff0d2332e5251048f2c80672b
Jonathan Célio <jonathan.clio@hotmail.com>
2021-11-24 00:27:04 -0300

    refactor: update layout component to use theme and update body class with theme mode

--- webapp/src/components/organisms/Layout/Layout.component.tsx
+++ webapp/src/components/organisms/Layout/Layout.component.tsx
@@ -1,8 +1,17 @@
 import { Header } from '../Header'
 import { Footer } from "../Footer";
 import { LayoutProps } from './Layout.props';
+import { useTheme } from '../../global';
+import React from 'react';
 
 export function Layout({ children }: LayoutProps) {
+  const { theme } = useTheme();
+
+  React.useEffect(() => {
+    const themeClassName = `${theme}-mode`;
+    document.querySelector('body').className = themeClassName;
+  }, [theme])
+
   return (
     <>
       <Header />
