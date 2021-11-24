e2c97fb85c697be1945189513275640407e51698
Jonathan Célio <jonathan.clio@hotmail.com>
2021-11-24 18:43:08 -0300

    feat: add favicon

--- webapp/src/pages/_app.tsx
+++ webapp/src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import "normalize.css";
 import "../components/global/styles/global.css";
+
+import Head from "next/head";
 import { UIProvider } from "../components/global";
 import { useRouter } from "next/router";
 import type { AppProps } from "next/app";
@@ -19,6 +21,26 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <UIProvider themeData={themeData} locale={{ locale, changeLocale }}>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <Component {...pageProps} />
     </UIProvider>
   );
