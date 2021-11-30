f8b3ab223ce44caa85a939781a6b924835d56b4a
Jonathan Célio <jonathan.clio@hotmail.com>
2021-11-30 17:50:45 -0300

    feat: change theme when user changes settings

--- webapp/src/components/organisms/Layout/Layout.component.tsx
+++ webapp/src/components/organisms/Layout/Layout.component.tsx
@@ -5,12 +5,29 @@ import { useTheme } from '../../global';
 import React from 'react';
 
 export function Layout({ children }: LayoutProps) {
-  const { theme } = useTheme();
+  const { theme, changeTheme } = useTheme();
 
   React.useEffect(() => {
     const themeClassName = `${theme}-mode`;
     document.querySelector('body').className = themeClassName;
   }, [theme])
+
+  const changeThemeByColorScheme = (event: MediaQueryListEvent) => {
+    const theme = event.matches ? "dark" : "light";
+    changeTheme(theme);
+  };
+
+  React.useEffect(() => {
+    window
+      .matchMedia("(prefers-color-scheme: dark)")
+      .addEventListener("change", changeThemeByColorScheme);
+
+    return () => {
+      window
+        .matchMedia("(prefers-color-scheme: dark)")
+        .removeEventListener("keydown", changeThemeByColorScheme);
+    };
+  });
 
   return (
     <>
