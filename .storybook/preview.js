87838aaea5933ca62fdcc3c9b5849b1689352860
Jonathan Célio <jonathan.clio@hotmail.com>
2022-01-16 00:38:46 -0300

    fix: add test ui provider as storybook decorator to show stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,14 @@
 import "normalize.css";
 import "../webapp/src/components/global/styles/global.css";
+import { TestUIProvider } from "../webapp/src/components/global/TestUIProvider";
+
+export const decorators = [
+  (Story) => (
+    <TestUIProvider>
+      <Story />
+    </TestUIProvider>
+  ),
+];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
