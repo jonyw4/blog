f7aa73a94aff6a5158eace57f80915b556b9e39e
Jonathan Célio <jonathan.clio@hotmail.com>
2022-01-16 00:41:11 -0300

    feat: add social media links in footer with new css classes

--- webapp/src/components/organisms/Footer/Footer.component.tsx
+++ webapp/src/components/organisms/Footer/Footer.component.tsx
@@ -3,6 +3,22 @@ export function Footer() {
   return (
     <footer>
       <LanguageSelect />
+
+      <ul
+        className="inline-list small strong"
+        style={{ marginBottom: "0.5em" }}
+      >
+        <li>
+          <a href="https://www.linkedin.com/in/jonycelio/">LinkedIn</a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/jonycelio/">Instagram</a>
+        </li>
+        <li>
+          <a href="https://lyrax.systems/">Lyrax</a>
+        </li>
+      </ul>
+
       <small>
         <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">
           CC BY-NC-SA 4.0 2021
